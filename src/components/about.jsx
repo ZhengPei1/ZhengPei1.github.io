d3f24e291d1fd988e74924e8a1637c424598b064
Zheng <15011325609pz@gmail.com>
2024-03-01 16:20:58 -0500

    half finished style

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -10,23 +10,7 @@ export default function About(){
             <p id = "aboutIntro">I'm <span className="myName">Zheng Pei</span>, a first year computer science student 
             studying at University of Waterloo.</p>
             
-            <a className="github" href = "https://github.com/ZhengPei1" target = "_blank" >
-                <button >
-                    Github
-                </button>
-            </a>
             
-            <a className="linkedin" href = "https://www.linkedin.com/in/zheng-pei/" target = "_blank" >
-                <button >
-                    LinkedIn
-                </button>
-            </a>
-
-            <a className="email" href = "mailto:z27pei@uwaterloo.ca">
-                <button >
-                    Email
-                </button>
-            </a>
 
         </div>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './style.css'
+import './assets/constant.css'
+import './assets/scrollbar.css'
+import './assets/navbar.css'
+import './assets/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
